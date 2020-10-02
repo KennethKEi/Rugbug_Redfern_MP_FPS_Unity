1530a288265169a1d23c702fc8435148d4f5b601
Kenneth_KEi <hotdinner@gmail.com>
2020-10-02 18:04:42 +1000

    Stuff up

--- Rugbug_Redfern_MP_FPS/Assets/Scripts/Launcher.cs
+++ Rugbug_Redfern_MP_FPS/Assets/Scripts/Launcher.cs
@@ -5,12 +5,24 @@ using Photon.Pun;
 using TMPro;
 using System.Runtime.InteropServices;
 using Photon.Pun.Demo.Cockpit;
+using Photon.Realtime;
+using System.Security.Cryptography;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+
+
+    private void Awake()
+    {
+        Instance = this; 
+    }
 
     void Start()
     {
@@ -59,8 +71,26 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("Loading");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+    }
+
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("Title");
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for(int i =0; i<roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
     }
 }
